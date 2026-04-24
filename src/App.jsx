d815293bcbb158d03ec694b6f853542db957e6e5
samaelbakry <samaabakry68@gmail.com>
2026-04-24 22:15:43 +0300

    feat: implement About section with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import About from "./components/sections/About";
 import Hero from "./components/sections/Hero";
 
 const App = () => {
 	return<>
 	<main className="min-h-screen w-screen relative overflow-x-hidden">
 		<Hero/>
-		<section className="min-h-screen bg-black">
-
-		</section>
+		<About/>
 	</main>
  	</>
 };
